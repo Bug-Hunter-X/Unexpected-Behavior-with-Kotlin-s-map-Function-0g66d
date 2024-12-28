8ae9f1553ad162d03c8cf5af61fb03764f3af4d2
BugHunter-X <askzenai@gmail.com>
2024-12-28 19:52:06 +0100

    Add bug.kt file

---
+++ bug.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5)
+    val doubledList = list.map { it * 2 }
+    println(doubledList)
+    //This code is correct. It doubles each element in the list.
+}
